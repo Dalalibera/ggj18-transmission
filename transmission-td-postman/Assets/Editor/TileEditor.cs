b28842825da51eede33fc7cd67cc8026e7c3f581
Herbert Weigert Venancio <hwvenancio@gmail.com>
2018-01-27 10:24:02 -0200

    created tile map using voxel mountains

---
+++ transmission-td-postman/Assets/Editor/TileEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TileData))]
+public class TileEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Recalculate Tiles"))
+        {
+            ((TileData)target).RecalculateTiles();
+        }
+    }
+}
