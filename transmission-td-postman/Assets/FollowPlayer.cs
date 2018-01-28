4d2a0636600c2295720c9f9338ed5bbe3d009f2a
Gypso <email@qualquer>
2018-01-28 02:12:10 -0200

    CHANGES A LOT OF SHIT, I HOPE IT DOESNT FUCK UP ANYTHING

--- transmission-td-postman/Assets/FollowPlayer.cs
+++ transmission-td-postman/Assets/FollowPlayer.cs
@@ -7,11 +7,19 @@ public class FollowPlayer : MonoBehaviour {
     public Vector3 playerDir;
     public Vector3 playerPrevPos;
     public float distance;
+    float cameraCorrection = 0;
     
     void LateUpdate() {
+
+
+        if (Input.GetMouseButton(0))
+        {
+            cameraCorrection += Input.GetAxis("Mouse Y");
+        }
+
         float desiredAngle = player.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, desiredAngle - 90, 0);
         transform.position = player.transform.position - (rotation * offset);
-        transform.LookAt(player.position + new Vector3(0, 3, 0));
+        transform.LookAt(player.position + new Vector3(0, cameraCorrection, 0));
     }
 }
